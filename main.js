a839e8900abd3654637e06d1c0e80a970290fb62
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-20 21:56:59 -0300

    Fotos dos users
    fotos dos users

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 const links = document.querySelectorAll("nav ul li a");
+const header = document.querySelector("#header");
 
 // Abre e fecha menu
 for (let element of toggle) {
@@ -9,8 +10,19 @@ for (let element of toggle) {
   });
 }
 
+//Rolagem do menu para os itens
 for (const link of links) {
   link.addEventListener("click", () => {
     nav.classList.remove("show");
   });
 }
+
+//Munda o header da pagina quando rolagem for ativada
+const navHeight = header.offsetHeight;
+addEventListener("scroll", () => {
+  if (scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
