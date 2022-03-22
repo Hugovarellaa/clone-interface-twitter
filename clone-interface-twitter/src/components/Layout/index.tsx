c0f6d4f7c0dab452b37250b71ff5f059364a64e0
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-22 15:24:27 -0300

    Component Menubar Finalizado

--- clone-interface-twitter/src/components/Layout/index.tsx
+++ clone-interface-twitter/src/components/Layout/index.tsx
@@ -1,11 +1,12 @@
 import { Main } from "../Main"
+import MenuBar from "../MenuBar"
 import { Container, Wrapper } from "./styles"
 
 export function Layout (){
   return (
     <Container>
       <Wrapper>
-        {/* <MenuBar /> */}
+        <MenuBar />
         <Main />
         {/* <Sidebar />  */}
       </Wrapper>
---
+++ clone-interface-twitter/src/components/MenuBar/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+import Button from "../Button";
+
+import {
+  Container,
+  TopSide,
+  Logo,
+  MenuButtons,
+  HomeIcon,
+  BellIcon,
+  EmailIcon,
+  FavoriteIcon,
+  ProfileIcon,
+  Botside,
+  Avatar,
+  ProfileData,
+  ExitIcon,
+} from "./styles";
+
+const MenuBar: React.FC = () => {
+  return (
+    <Container>
+      <TopSide>
+        <Logo />
+
+        <MenuButtons>
+          <HomeIcon />
+          <span>Página Inicial</span>
+        </MenuButtons>
+
+        <MenuButtons>
+          <BellIcon />
+          <span>Notificações</span>
+        </MenuButtons>
+
+        <MenuButtons>
+          <EmailIcon />
+          <span>Menssagens</span>
+        </MenuButtons>
+
+        <MenuButtons>
+          <FavoriteIcon />
+          <span>Favoritados</span>
+        </MenuButtons>
+
+        <MenuButtons className="active">
+          <ProfileIcon />
+          <span>Perfil</span>
+        </MenuButtons>
+
+        <Button>
+          <span>Tweetar</span>
+        </Button>
+      </TopSide>
+
+      <Botside>
+        <Avatar />
+        <ProfileData>
+          <strong>Hugo Varella</strong>
+          <span>@hugovarellaa</span>
+        </ProfileData>
+        <ExitIcon />
+      </Botside>
+    </Container>
+  );
+};
+
+export default MenuBar;
